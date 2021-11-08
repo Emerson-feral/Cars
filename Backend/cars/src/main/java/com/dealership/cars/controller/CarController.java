5640a5db0db37da5e80863d87d2ebfc77b90dfcc
Emerson Fernandes <emerson_feral@hotmail.com>
2021-11-08 17:03:02 +0100

    Creating controller routes

--- Backend/cars/src/main/java/com/dealership/cars/controller/CarController.java
+++ Backend/cars/src/main/java/com/dealership/cars/controller/CarController.java
@@ -5,7 +5,11 @@ import com.dealership.cars.entity.Car;
 import com.dealership.cars.service.CarService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,28 @@ public class CarController {
     public Car addCar(@RequestBody Car car){
         return service.saveCar(car);
     }
+
+    @GetMapping("/Car/{id}")
+    public Car findCarById(@PathVariable Integer id){
+        return service.getCarById(id);
+    }
+
+    @GetMapping("/Car/{model}")
+    public Car findCarByModel(@PathVariable String model){
+        return service.getCarByModel(model);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public String deleteCar(@PathVariable Integer id){
+        return service.deleteCar(id);
+    }
+
+    @PutMapping("/update")
+    public Car updateCar(@RequestBody Car car){
+        return service.disableCarAttribute(car);
+    }
+
+    
+
 
 }
