765aedb4f0ff15d03ce78b3fc723e5bdc0d1df33
Emerson Fernandes <emerson_feral@hotmail.com>
2022-01-24 13:39:55 +0100

    Brand entity

---
+++ BackendCars/cars/src/main/java/com/dealership/cars/entity/Brand.java
@@ -0,0 +1,49 @@
+package com.dealership.cars.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Brand")
+public class Brand {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+
+
+    public Brand() {
+    
+    }
+    
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+
+}
