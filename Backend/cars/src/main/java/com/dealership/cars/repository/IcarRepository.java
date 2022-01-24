29f7fffe81e4483d6143836c08ae127a45c6b93a
Emerson Fernandes <emerson_feral@hotmail.com>
2022-01-24 13:36:46 +0100

    Removed files

--- Backend/cars/src/main/java/com/dealership/cars/repository/IcarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dealership.cars.repository;
-
-import com.dealership.cars.entity.Car;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IcarRepository extends CrudRepository<Car,Integer> {
-    Car findByModel(String model);
-}
